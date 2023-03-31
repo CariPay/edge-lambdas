fe6a4fb5752ba195dc55c0dd2d664395375e08e0
Darion Hernandez <darionhernandez868@gmail.com>
2023-03-30 20:16:24 -0400

    Updated deployment to use ssm, updated runtime

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ $TRAVIS_BRANCH =~ develop|feature ]]; then
-  echo 'Deploying to dev-1: ' $TRAVIS_BRANCH
-  STAGE=dev-1
+  echo 'Deploying to dev: ' $TRAVIS_BRANCH
+  STAGE=dev
 elif [[ $TRAVIS_BRANCH =~ bug|release ]]; then
   echo 'Deploying to qa: ' $TRAVIS_BRANCH
   STAGE=qa
@@ -17,20 +17,8 @@ fi
 COMPANY_INITIALS=$(deploy/get_company.sh $TRAVIS_BRANCH)
 REGION="${AWS_REGION:=us-east-1}"
 
-echo "Fetching config for $COMPANY_INITIALS on stage $STAGE"
-aws s3 cp s3://kyc-config/$COMPANY_INITIALS/edge-lambdas/env/$STAGE.env ./config.env --region $REGION
-
-echo "export DEPLOYMENT_STAGE=$STAGE" >> config.env
-echo "export STAGE=$STAGE" >> config.env
-
-source config.env
-
-echo "config values"
-cat config.env
-echo "====COMPLETE===="
-
 npm install
 
-serverless deploy -s $STAGE
+serverless deploy -s $STAGE --region $REGION
 
 echo 'Deployed.'
