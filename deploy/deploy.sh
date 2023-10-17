098469db52398b86c8850403c530f099b30f0c75
ricardofelix2@yahoo.com <ricardofelix2@yahoo.com>
2023-10-17 10:50:25 -0400

    BRANCH_NAME

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-if [[ $GITHUB_REF_NAME =~ develop|feature ]]; then
-  echo 'Deploying to dev: ' $GITHUB_REF_NAME
+if [[ $BRANCH_NAME =~ develop|feature ]]; then
+  echo 'Deploying to dev: ' $BRANCH_NAME
   STAGE=dev
-elif [[ $GITHUB_REF_NAME =~ bug|release ]]; then
-  echo 'Deploying to qa: ' $GITHUB_REF_NAME
+elif [[ $BRANCH_NAME =~ bug|release ]]; then
+  echo 'Deploying to qa: ' $BRANCH_NAME
   STAGE=qa
-elif [[ $GITHUB_REF_NAME =~ master ]]; then
-  echo 'Deploying to prod: ' $GITHUB_REF_NAME
+elif [[ $BRANCH_NAME =~ master ]]; then
+  echo 'Deploying to prod: ' $BRANCH_NAME
   STAGE=prod
 else
-  echo 'Deploying to dev, no matching branch found: ' $GITHUB_REF_NAME
+  echo 'Deploying to dev, no matching branch found: ' $BRANCH_NAME
   STAGE=dev
 fi
 
-COMPANY_INITIALS=$(deploy/get_company.sh $GITHUB_REF_NAME)
+COMPANY_INITIALS=$(deploy/get_company.sh $BRANCH_NAME)
 REGION="${AWS_REGION:=us-east-1}"
 
 npm install
